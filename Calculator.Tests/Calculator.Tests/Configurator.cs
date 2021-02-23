a90c1e37961f36202017c854ba208571f848637d
Daniils <daniils.korolenko@if.lv>
2021-02-23 10:51:43 +0200

    Adjusted exception messages

--- Calculator.Tests/Calculator.Tests/Configurator.cs
+++ Calculator.Tests/Calculator.Tests/Configurator.cs
@@ -23,7 +23,7 @@ namespace Calculator.Tests
             }
             catch(Exception ex)
             {
-                throw;
+                throw new InvalidOperationException("resultName must be of the type AccessibilityId");
             }
         }
 
@@ -44,7 +44,7 @@ namespace Calculator.Tests
             }
             catch(Exception ex)
             {
-                throw;
+                throw new InvalidOperationException("type must be of the type element name");
             }
 
         }
