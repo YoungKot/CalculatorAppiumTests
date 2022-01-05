51494157ae2936407c4098a36451b00434a92c00
Daniils K <internalpainy@gmail.com>
2022-01-05 21:18:06 +0200

    Remove unnecessary comments

--- Calculator.Tests/Calculator.Tests/Scenarios/ScenarioStandard.cs
+++ Calculator.Tests/Calculator.Tests/Scenarios/ScenarioStandard.cs
@@ -19,28 +19,24 @@ namespace Calculator.Tests
         [Test]
         public void AdditionTest()
         {
-            // Find the buttons by their names and click them in sequence to perform 3 + 8 = 11
             scenarioStandardPage.Addition();
         }
 
         [Test]
         public void SubtractionTest()
         {
-            // Find the buttons by their accessibility ids and click them in sequence to perform 8 - 7 = 1
             scenarioStandardPage.Subtraction();
         }
 
         [Test]
         public void DivisionTest()
         {
-            // Find the buttons by their names using XPath and click them in sequence to perform 55 / 11 = 5
             scenarioStandardPage.Division();
         }
 
         [Test]
         public void MultiplicationTest()
         {
-            // Find the buttons by their names and click them in sequence to perform 3 * 8 = 24
             scenarioStandardPage.Multiplication();
         }
     }
