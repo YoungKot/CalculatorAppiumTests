5f85e5ae40064f1dccb25fc933eaab805e80cc5a
Daniils K <internalpainy@gmail.com>
2023-05-12 23:24:20 +0300

    Improves encapsulation

--- Calculator.Tests/Calculator.Tests/Scenarios/ScenarioDateCalculation.cs
+++ Calculator.Tests/Calculator.Tests/Scenarios/ScenarioDateCalculation.cs
@@ -10,7 +10,7 @@ namespace Calculator.Tests
 
         private Configurator _config;
 
-        private reaonly string calcType = "Date Calculation Calculator";
+        private readonly string calcType = "Date Calculation Calculator";
 
         [OneTimeSetUp]
         public void SetUp()
